b099bfb8694575ed23668b4ae7c01310db8f1005
Nikolai.Volokhan <volohan1@mail.ru>
2023-05-26 12:08:03 +0500

    It works, the answer did not converge

--- STI.py
+++ STI.py
@@ -1,25 +1,45 @@
+from scipy.integrate import solve_ivp, quad, simps
+import pickle
 import numpy as np
 import HIV
-from scipy.integrate import solve_ivp
+import graphs
 
 ulb = np.array([0, 0])
 urb = np.array([0.7, 0.3])
 x0 = [163573, 5, 11945, 46, 63919, 24]
 fh = [HIV.T1, HIV.T2, HIV.I1, HIV.I2, HIV.V, HIV.E]
+Q = 0.1
+R1 = 20000
+R2 = 20000
+S = 1000
 s = 5
 l = 20
-tf = 400
+tf = 1000
 
 
-def _recursive(tz, x, u, u_space, res):
+def _opt_J(t, Vt, Et, u):
+    f_Vt = simps(Vt, t)
+    f_Et = simps(Et, t)
+    J = Q * f_Vt - S * f_Et
+    for t_s in range(int(t[0]), int(t[-1]), s):
+        t_u = int((t_s % l) / s)
+        integral, _ = quad(lambda x: R1 * u[t_u][0] + R2 * u[t_u][1], t_s,
+                           t_s + s)
+        J += integral
+    return J, u
+
+
+def _recursive(tz, t, x, u, u_space, res):
     if len(u) == l / s:
-        res.append([x, u])
+        res.append([t, x[:, 1:], u])
     else:
         for i in range(u_space.shape[0]):
             sol = solve_ivp(
                 fun=lambda t, y: [fi(t, *y, *u_space[i, :]) for fi in fh],
-                t_span=(tz, tz + s), y0=x, method='BDF')
-            _recursive(tz + s, sol.y[:, -1], [*u, u_space[i, :]], u_space, res)
+                t_span=(tz, tz + s), y0=x[:, -1], method='BDF')
+            _recursive(tz + s, [*t, *sol.t[1:]],
+                       np.column_stack((x, sol.y[:, 1:])), [*u, u_space[i, :]],
+                       u_space, res)
     return
 
 
@@ -36,10 +56,43 @@ def STI():
             u[u == 0] = ulb[u_idx]
         u_space[i, :] = np.transpose(u_buf)
 
-    x = x0.copy()
+    J_min = 0
+    result = [[[], np.column_stack((x0.copy(), x0.copy())), []]]
     for t in range(0, tf, l):
+        print(t)
         res = []
-        _recursive(t, x0, [], u_space, res)
+        _recursive(t, [t], np.column_stack(
+            (result[-1][1][:, -1], result[-1][1][:, -1])), [], u_space, res)
+        J_res = [_opt_J(r[0], r[1][4, :], r[1][5, :], r[2]) for r in res]
+        min_J = min(J_res)
+        d = {(0.0, 0.0): '0', (0.7, 0.0): '1', (0.0, 0.3): '2',
+             (0.7, 0.3): '3'}
+        t = int(''.join([d[(i[0], i[1])] for i in min_J[1]]), 4)
+        result.append(res[t])
+        J_min += min_J[0]
+
+    result = result[1:]
+    with open('STI.pickle', 'wb') as f:
+        pickle.dump(result, f)
+
+    t = [0]
+    f1 = [x0[0]]
+    f2 = [x0[1]]
+    f3 = [x0[2]]
+    f4 = [x0[3]]
+    f5 = [x0[4]]
+    f6 = [x0[5]]
+    for i in result:
+        t.extend(i[0][1:])
+        f1.extend(i[1][0, 1:])
+        f2.extend(i[1][1, 1:])
+        f3.extend(i[1][2, 1:])
+        f4.extend(i[1][3, 1:])
+        f5.extend(i[1][4, 1:])
+        f6.extend(i[1][5, 1:])
+
+    print(J_min)
+    graphs.draw(t, f1, f2, f3, f4, f5, f6)
 
 
 STI()
---
+++ graphs.py
@@ -0,0 +1,53 @@
+from matplotlib import pyplot as plt
+from HIV import T1, T2, I1, I2, V, E, start, t0, tn, h
+from RK4 import RK4
+
+
+def draw(t, f1, f2, f3, f4, f5, f6):
+    x, y = RK4([T1, T2, I1, I2, V, E], start, t0, tn, h)
+    # создаем объект фигуры и определяем размер
+    fig = plt.figure(figsize=(10, 8))
+
+    # добавляем первый подграфик
+    ax1 = fig.add_subplot(3, 2, 1)  # 2 строки, 3 столбца, 1-ый подграфик
+    ax1.plot(t, f1)
+    ax1.plot(x, [yi[0] for yi in y], color='red', linestyle='dashed')
+    ax1.set_title('T1')
+    ax1.set_yscale('log')
+
+    # добавляем второй подграфик
+    ax2 = fig.add_subplot(3, 2, 2)  # 2 строки, 3 столбца, 2-ой подграфик
+    ax2.plot(t, f2)
+    ax2.plot(x, [yi[1] for yi in y], color='red', linestyle='dashed')
+    ax2.set_title('T2')
+    ax2.set_yscale('log')
+
+    # добавляем третий подграфик
+    ax3 = fig.add_subplot(3, 2, 3)  # 2 строки, 3 столбца, 3-ий подграфик
+    ax3.plot(t, f3)
+    ax3.plot(x, [yi[2] for yi in y], color='red', linestyle='dashed')
+    ax3.set_title('I1')
+    ax3.set_yscale('log')
+
+    # добавляем четвертый подграфик
+    ax4 = fig.add_subplot(3, 2, 4)  # 2 строки, 3 столбца, 4-ый подграфик
+    ax4.plot(t, f4)
+    ax4.plot(x, [yi[3] for yi in y], color='red', linestyle='dashed')
+    ax4.set_title('I2')
+    ax4.set_yscale('log')
+
+    # добавляем пятый подграфик
+    ax5 = fig.add_subplot(3, 2, 5)  # 2 строки, 3 столбца, 5-ый подграфик
+    ax5.plot(t, f5)
+    ax5.plot(x, [yi[4] for yi in y], color='red', linestyle='dashed')
+    ax5.set_title('V')
+    ax5.set_yscale('log')
+
+    # добавляем шестой подграфик
+    ax6 = fig.add_subplot(3, 2, 6)  # 2 строки, 3 столбца, 6-ой подграфик
+    ax6.plot(t, f6)
+    ax6.plot(x, [yi[5] for yi in y], color='red', linestyle='dashed')
+    ax6.set_title('E')
+    ax6.set_yscale('log')
+
+    plt.show()
